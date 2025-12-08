087868fa0dd9f4c0b879261aa73ac05402cd5d95
karthik balasubramanian <karthikbalasubramanian08@gmail.com>
2025-12-08 22:06:00 +0530

    fix: update singleton pattern in accordance to prisma v7

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,13 +1,21 @@
 import { PrismaClient } from './generated/prisma/client.js';
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 
-declare global {
-  var prismaClient: PrismaClient | undefined;
+const createPrismaClient = () => {
+  // pool using the POOLED URL (DATABASE_URL)
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    max: 2 //pool size for serverless 
+  })
+
+  const adapter = new PrismaPg(pool)
+
+  return new PrismaClient({ adapter })
 }
 
-const prisma = global.prismaClient || new (PrismaClient as unknown as new () => PrismaClient)();
+const globalForPrisma = globalThis as unknown as { prisma: ReturnType<typeof createPrismaClient> | undefined }
 
-if (process.env.NODE_ENV !== 'production') {
-  global.prismaClient = prisma;
-}
+export const prisma = globalForPrisma.prisma || createPrismaClient()
 
-export default prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma; // singleton pattern.
